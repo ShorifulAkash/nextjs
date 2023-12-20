a913a619f932f73c7049b7461f10751a2e5931ba
shorifulislam99 <shoriful3522@gmail.com>
2023-12-20 12:19:06 +0600

    link update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,36 +1,55 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
+
 export const Header = () => {
-  return (
-    <header className="bg-gray-400 py-4">
-        <div className="lg:container max-w-7xl px-4 sm:px-6 md:px-10 lg:px-14 xl:px-24 mx-auto">
-            <div className="flex items-center justify-between gap-10">
-                <div className="">
-                    <Link href="/">Logo</Link>
-                </div>
-                <ul className="text-base text-gray-100 font-medium flex items-center gap-4">
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link href="/portfolio">Portfolio</Link>
-                    </li>
-                    <li>
-                        <Link href="/blog">Blog</Link>
-                    </li>
-                    <li>
-                        <Link href="/contact">Contact</Link>
-                    </li>
-                </ul>
-                <div className="">
-                    <Link href="/discover">Discover</Link>
+    const pathname = usePathname()
+
+
+    return (
+        <header className="bg-gray-400 py-4">
+            <div className="lg:container max-w-7xl px-4 sm:px-6 md:px-10 lg:px-14 xl:px-24 mx-auto">
+                <div className="flex items-center justify-between gap-10">
+                    <div className="">
+                        <Link href="/">Logo</Link>
+                    </div>
+                    <ul className="text-base text-gray-100 font-medium flex items-center gap-4">
+                        <li>
+                            <Link href="/" className={`link ${pathname === '/' ? 'active' : ''}`}>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/about" className={`link ${pathname === '/about' ? 'active' : ''}`}>
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/portfolio" className={`link ${pathname === '/portfolio' ? 'active' : ''}`}>
+                                Portfolio
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/blog" className={`link ${pathname === '/blog' ? 'active' : ''}`}>
+                                Blog
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/contact" className={`link ${pathname === '/contact' ? 'active' : ''}`}>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                    <div className="">
+                        <Link href="/discover" className={`link ${pathname === '/discover' ? 'active' : ''}`}>
+                            Discover
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
-  )
+        </header>
+    )
 }
